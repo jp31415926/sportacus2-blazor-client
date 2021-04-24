bbb230e6cb348fbe54b04efdf2f776645cc40b37
Peter McCoy <pmccoy@stgp.com>
2021-04-24 12:56:26 -0500

    Test Blazor App

---
+++ TestBlazorApp/Data/SoccerGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazorApp.Data
+{
+    public class SoccerGame
+    {
+        public int GameId { get; set; }
+        public string Project { get; set; }
+        public string Region { get; set; }
+        public DateTime GameTime { get; set; }
+        public string Status { get; set; }
+        public Venue GameVenue { get; set; }
+        public int GameLength { get; set; }
+        public int SlotLenth { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string Official { get; set; }
+
+    }
+
+    public class Venue
+    {
+        public Guid id { get; set; }
+        public string RegionCode { get; set; }
+        public string Location { get; set; }
+        public string FieldId { get; set; }
+        public string SubField { get; set; }
+
+        private string _fullFieldName;
+
+        public string FullFieldName
+        {
+            get { return $"{RegionCode}-{Location}{FieldId}{SubField}"; }
+            set { _fullFieldName = value; }
+        }
+
+    }
+}
---
+++ TestBlazorApp/Data/SoccerGameService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazorApp.Data
+{
+    public class SoccerGameService
+    {
+        public Task<List<SoccerGame>> GetSoccerGamesAsync(DateTime startDate)
+        {
+            SoccerGame newGame = new SoccerGame()
+            {
+                GameId = 1,
+                Project = "Spring2021",
+                Region = "R0894",
+                GameTime = DateTime.Now,
+                Status = "Open",
+                GameVenue = new Venue()
+                {
+                    id = new Guid(),
+                    RegionCode = "R0894",
+                    Location = "MMS",
+                    FieldId = "01",
+                    SubField = ""
+                },
+                GameLength = 90,
+                SlotLenth = 120,
+                HomeTeam = "R0894-19UB-01",
+                AwayTeam = "R0498-19UB-02",
+                Official = "Center Referee"
+            };
+
+            List<SoccerGame> Games = new List<SoccerGame>();
+            Games.Add(newGame);
+            return Task.FromResult(Games);
+        }
+    }
+}
